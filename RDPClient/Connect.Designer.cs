66ec1e973bb11027c543d262338b91520596aab2
Mark_Pr <markprivodnov@gmail.com>
2024-09-03 15:57:11 +0700

    Client application add

---
+++ RDPClient/Connect.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace RDPClient
+{
+    partial class Connect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textRemote = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textRemote
+            // 
+            this.textRemote.Location = new System.Drawing.Point(13, 13);
+            this.textRemote.Name = "textRemote";
+            this.textRemote.Size = new System.Drawing.Size(266, 20);
+            this.textRemote.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Connect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 114);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textRemote);
+            this.Name = "Connect";
+            this.Text = "Connect";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textRemote;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ RDPClient/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RDPClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1(IPAddress address)
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
